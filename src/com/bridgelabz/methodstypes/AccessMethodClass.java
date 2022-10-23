9e855995669408377edf31850ee16a94a4638e3e
usha-12 <usha12ahirwar@gmail.com>
2022-10-23 16:19:38 +0530

    How to call static and non-static method

--- src/com/bridgelabz/methodstypes/AccessMethodClass.java
+++ src/com/bridgelabz/methodstypes/AccessMethodClass.java
@@ -1,2 +1,10 @@
-package com.bridgelabz.methodstypes;public class AccessMethodClass {
+package com.bridgelabz.methodstypes;
+
+public class AccessMethodClass {
+    public static void main(String[] args) {
+        Method1MainClass obj2= new  Method1MainClass();
+        System.out.println(obj2.y);
+        obj2.method2();
+        Method1MainClass.method1();//call outside class static method using class name
+    }
 }
--- src/com/bridgelabz/methodstypes/Method1MainClass.java
+++ src/com/bridgelabz/methodstypes/Method1MainClass.java
@@ -1,2 +1,42 @@
-package com.bridgelabz.methodstypes;public class Method1MainClass {
+package com.bridgelabz.methodstypes;
+
+public class Method1MainClass {
+    static int x =100;//static variable
+    static int y = 200;// instance variable
+    static void method1(){
+        int y = 400;
+        System.out.println("Static method called ");
+        System.out.println(y);//if local and instance variable name same then if access i have access directly reference given to be local variable
+        Method1MainClass obj1 = new Method1MainClass();
+        System.out.println(obj1.x);
+    }
+    void method3(){
+        method1();
+        System.out.println("Non-static method 2");
+    }
+    void method2(){
+        method1();
+        method3();//if we want to access non-method in non-method then we don't want to create again another method
+        // because we are also create a method for non-static method2
+        System.out.println("Non-static Method Called ");
+        System.out.println(y);//access static variable within a class
+        System.out.println(x);//also create a object for accessing non-static variable so don't need to create 2 time obj
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome method program");
+        Method1MainClass obj1 = new Method1MainClass();
+        System.out.println("Instance variable print");
+        System.out.println(obj1.x);//access instance variable
+        System.out.println("Method2 non-static method print");
+        obj1.method2();//call non-static method
+        System.out.println("method1 print static");
+        method1();//call static method (we don't want obj  to call static method)
+        System.out.println("print static variable");
+        System.out.println(y);
+        obj1.x = 500;
+        System.out.println(obj1.x);
+
+
+    }
 }
